a77ab550657e1a018fa2d4d2bd90eb2af527b77f
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-07 10:45:22 +0200

    Complete js mini task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    if (firstNum<0 || 
+        secondNum<0 ||
+        typeof firstNum != 'number' ||
+        typeof secondNum != 'number') {
+            return 'ERROR';
+    }
+
+    minNum = Math.min(firstNum, secondNum);
+    maxNum = Math.max(firstNum, secondNum);
+    let sum = 0;
+    for (let i = minNum; i <= maxNum; i++) {
+        sum += i;        
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
