12713fc6b996d88195626b173a981af41608baaa
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-07 10:24:53 +0200

    Complete mini js mini task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(mainArray, ...toBeRemoved) {
+    for(const item of toBeRemoved){
+        let index = mainArray.indexOf(item);
+        if (index > -1) {
+            mainArray.splice(index, 1);
+        }
+    }
 
+    return mainArray;
 };
 
 // Do not edit below this line
