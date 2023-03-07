358fa9c6aa65437351bceb23f1db689712d2fad1
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-07 09:21:14 +0200

    Complete js mini task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, iterations) {
+    
+    if (iterations<0) {
+        return 'ERROR'        
+    }
+    let strings = '';
+    for (let i = 0; i < iterations; i++) {
+        strings += str;
+    }
+    return strings;
 };
 
 // Do not edit below this line
