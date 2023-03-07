cf001da8e77c63faabc92eca72573ff2e7b0dbf5
NduvhoEdward <71088485+NduvhoEdward@users.noreply.github.com>
2023-03-07 09:33:53 +0200

    Complete js mini task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let revStr = '';
+    for (let i = str.length; i > 0; i--) {
+        revStr += str[i-1];
+    }
 
+    return revStr;
 };
 
 // Do not edit below this line
